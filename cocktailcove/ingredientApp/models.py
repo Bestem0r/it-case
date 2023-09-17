53c098f6983177133934d58b47542bc21b9b8b6c
Jonas de Luna Skulberg <jonasdls@gmail.com>
2023-09-17 14:16:38 +0200

    Update name field to be unique

--- cocktailcove/ingredientApp/models.py
+++ cocktailcove/ingredientApp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class Ingredient(models.Model):
-    name = models.CharField(max_length=50, blank=False, null=False)
+    name = models.CharField(max_length=50, blank=False, null=False, unique=True)
     amount = models.DecimalField(default=0, max_digits=5, decimal_places=2, null=False)
 
     class Meta:
