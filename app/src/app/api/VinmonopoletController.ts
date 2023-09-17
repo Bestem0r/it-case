94c2025d83481147d907f8c734c886d0c5f43626
Jonas de Luna Skulberg <jonasdls@gmail.com>
2023-09-17 15:59:31 +0200

    Add VP api calls

---
+++ app/src/app/api/VinmonopoletController.ts
@@ -0,0 +1,32 @@
+async function getProductsLike(shortName: string) {
+  const products = fetch(
+    `https://apis.vinmonopolet.no/products/v0/details-normal?productShortNameContains=${shortName}`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "Cache-Control": "no-cache",
+        "Ocp-Apim-Subscription-Key": "9ebb6ab93fa24c9db24e26832e024834",
+      },
+    }
+  )
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      return data;
+    })
+    .catch((error) => {
+      console.error(
+        "There was a problem with the fetch operation:",
+        error.message
+      );
+    });
+
+  return products;
+}
+
+export { getProductsLike };
