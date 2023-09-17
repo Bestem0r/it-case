57395a650100487386ccc2c6accef61d9ccd98b2
haukkagu <falk.bruskeland@gmail.com>
2023-09-18 00:06:14 +0200

    Add default styling to modal

--- app/src/app/components/DrinkList.tsx
+++ app/src/app/components/DrinkList.tsx
@@ -1,9 +1,9 @@
-import { Dispatch, useEffect, useState } from "react";
+import { Dispatch, Fragment, useEffect, useState } from "react";
 import { Cocktail, Ingredient } from "../constants/types";
 import { fetchCocktailsAny } from "@/api/fetchCocktails";
 import styles from "./Recipes.module.css";
 import { ArrowForward } from "react-ionicons";
-import { Dialog } from "@headlessui/react";
+import { Transition, Dialog } from "@headlessui/react";
 
 interface AddIngredientsProps {
   generateDrinks: boolean;
@@ -60,20 +60,83 @@ const DrinkCard = ({ drink, setIsOpen, setCurrentDrink }: DrinkCardProps) => {
 };
 
 const DrinkModal = ({ drink, isOpen, setIsOpen }: DrinkModalProps) => {
+
+  function closeModal() {
+    setIsOpen(false)
+  }
+
+  function openModal() {
+    setIsOpen(true)
+  }
+
   return (
-    <Dialog open={isOpen} onClose={() => setIsOpen(false)}>
-      <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
-      <Dialog.Panel>
-        <Dialog.Title>{drink?.name}</Dialog.Title>
-        <Dialog.Description>{drink?.instructions}</Dialog.Description>
+    <>
+      <div className="fixed inset-0 flex items-center justify-center">
+        <button
+          type="button"
+          onClick={openModal}
+          className="rounded-md bg-black bg-opacity-20 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+        >
+          Open dialog
+        </button>
+      </div>
 
-        <p></p>
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={closeModal}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black bg-opacity-25" />
+          </Transition.Child>
 
-        <button onClick={() => setIsOpen(false)}>Deactivate</button>
-        <button onClick={() => setIsOpen(false)}>Cancel</button>
-      </Dialog.Panel>
-    </Dialog>
-  );
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                  <Dialog.Title
+                    as="h3"
+                    className="text-lg font-medium leading-6 text-gray-900"
+                  >
+                    Payment successful
+                  </Dialog.Title>
+                  <div className="mt-2">
+                    <p className="text-sm text-gray-500">
+                      Your payment has been successfully submitted. We’ve sent
+                      you an email with all of the details of your order.
+                    </p>
+                  </div>
+
+                  <div className="mt-4">
+                    <button
+                      type="button"
+                      className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                      onClick={closeModal}
+                    >
+                      Got it, thanks!
+                    </button>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
+    </>
+  )
 };
 
 const DrinkList = ({
