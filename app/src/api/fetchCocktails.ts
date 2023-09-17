e64139294a6024ff8d9e9ceca063c6f401022fa9
Falk Elvedal Bruskeland <66320400+haukkagu@users.noreply.github.com>
2023-09-17 15:17:31 +0200

    Add data fetching from api-ninjas API (#5)

---
+++ app/src/api/fetchCocktails.ts
@@ -0,0 +1,46 @@
+const API_KEY = "7d1tR1OEfdlpqqt8lE6cjQ==APKc86iendGIjdmB";
+const API_URL = `https://api.api-ninjas.com/v1/cocktail`;
+
+function getCombinations<T>(arr: T[]): T[][] {
+  const result: T[][] = [];
+  
+  function combine(current: T[], start: number) {
+    result.push([...current]);
+
+    for (let i = start; i < arr.length; i++) {
+      current.push(arr[i]);
+      combine(current, i + 1);
+      current.pop();
+    }
+  }
+  
+  combine([], 0);
+  result.shift();
+  return result;
+}
+
+export async function fetchCocktailsAll(ingredients) {
+    const response = await fetch(`${API_URL}?ingredients=${ingredients.join(',')}`, {
+      headers: {
+        'X-Api-Key': API_KEY
+      }
+    });
+    return response.json();
+}
+
+export async function fetchCocktailsAny(ingredients) {
+  const combinations = getCombinations(ingredients); 
+
+  const promises = combinations.map((combo) => fetchCocktailsAll(combo));;
+
+  const responses = await Promise.all(promises);
+
+  const uniqueCocktailNames = new Set();
+  const filterDuplicateDrinks = (cocktail: any) => {
+    if (uniqueCocktailNames.has(cocktail.name)) return false;
+    uniqueCocktailNames.add(cocktail.name);
+    return true;
+  }
+
+  return responses.flat().filter(filterDuplicateDrinks);
+}
