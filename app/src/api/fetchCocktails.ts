44f3ac866223ffaa718de918b38495815aa5ef3c
Falk Elvedal Bruskeland <66320400+haukkagu@users.noreply.github.com>
2023-09-17 18:10:26 +0200

    Change cocktail fetch API to thedocktaildb (#8)

--- app/src/api/fetchCocktails.ts
+++ app/src/api/fetchCocktails.ts
@@ -1,48 +1,66 @@
-import Ingredient from "@/app/constants/types";
+import {Cocktail, CocktailIngredient, Ingredient} from "@/app/constants/types";
 
-const API_KEY = "7d1tR1OEfdlpqqt8lE6cjQ==APKc86iendGIjdmB";
-const API_URL = `https://api.api-ninjas.com/v1/cocktail`;
+const API_KEY = "9973533";
+const API_URL = `https://www.thecocktaildb.com/api/json/v2/${API_KEY}/`;
 
-function getCombinations<T>(arr: T[]): T[][] {
-  const result: T[][] = [];
-  
-  function combine(current: T[], start: number) {
-    result.push([...current]);
+function getRandomInt(min: number, max: number): number {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-    for (let i = start; i < arr.length; i++) {
-      current.push(arr[i]);
-      combine(current, i + 1);
-      current.pop();
-    }
+function pickRandomItems<T>(list: T[], n: number): T[] {
+  const result: T[] = [...list];
+  const pickedItems: T[] = [];
+
+  for (let i = result.length - 1; i >= 0 && pickedItems.length < n; i--) {
+    const randomIndex = getRandomInt(0, i);
+    [result[i], result[randomIndex]] = [result[randomIndex], result[i]];
+    pickedItems.push(result[i]);
   }
-  
-  combine([], 0);
-  result.shift();
-  return result;
+
+  return pickedItems;
 }
 
-export async function fetchCocktailsAll(ingredients: Ingredient[]) {
-    const response = await fetch(`${API_URL}?ingredients=${ingredients.map(ingredient => ingredient.name).join(',')}`, {
-      headers: {
-        'X-Api-Key': API_KEY
-      }
-    });
-    return response.json();
+async function populateCocktail(cocktail: any): Promise<Cocktail> {
+  const response = await fetch(`${API_URL}lookup.php?i=${cocktail.idDrink}`);
+  const data = (await response.json())["drinks"][0];
+
+  return {
+    name: data.strDrink,
+    thumbnail: data.strDrinkThumb,
+    instructions: data.strInstructions,
+    ingredients: [
+      { name: data.strIngredient1, amount: data.strMeasure1},
+      { name: data.strIngredient2, amount: data.strMeasure2},
+      { name: data.strIngredient3, amount: data.strMeasure3},
+      { name: data.strIngredient4, amount: data.strMeasure4},
+      { name: data.strIngredient5, amount: data.strMeasure5},
+      { name: data.strIngredient6, amount: data.strMeasure6},
+      { name: data.strIngredient7, amount: data.strMeasure7},
+      { name: data.strIngredient8, amount: data.strMeasure8},
+      { name: data.strIngredient9, amount: data.strMeasure9},
+      { name: data.strIngredient10, amount: data.strMeasure10},
+      { name: data.strIngredient11, amount: data.strMeasure11},
+      { name: data.strIngredient12, amount: data.strMeasure12},
+      { name: data.strIngredient13, amount: data.strMeasure13},
+      { name: data.strIngredient14, amount: data.strMeasure14},
+      { name: data.strIngredient15, amount: data.strMeasure15}
+    ] as CocktailIngredient[]
+  };
 }
 
-export async function fetchCocktailsAny(ingredients: Ingredient[]) {
-  const combinations = getCombinations(ingredients); 
-
-  const promises = combinations.map((combo) => fetchCocktailsAll(combo));;
-
+async function populateCocktails(cocktails: any[]) {
+  const promises = cocktails.map(cocktail => populateCocktail(cocktail));
   const responses = await Promise.all(promises);
+  return responses;
+}
 
-  const uniqueCocktailNames = new Set();
-  const filterDuplicateDrinks = (cocktail: any) => {
-    if (uniqueCocktailNames.has(cocktail.name)) return false;
-    uniqueCocktailNames.add(cocktail.name);
-    return true;
-  }
+async function fetchCocktailsAll(ingredients: Ingredient[]): Promise<Cocktail[]> {
+    const response = await fetch(`${API_URL}filter.php?i=${ingredients.map(ingredient => ingredient.name).join(',')}`);
+    const cocktails = (await response.json())["drinks"] as any[];
+    const randomCocktails = pickRandomItems(cocktails, 10);
+    return populateCocktails(randomCocktails);
+}
 
-  return responses.flat().filter(filterDuplicateDrinks);
+export async function fetchCocktailsAny(ingredients: Ingredient[]): Promise<Cocktail[]>{
+  return fetchCocktailsAll(ingredients);
 }
